a6a08eceb4d6667bd8e85ed8e5e83e4f47b9379b
DanielPuertaM <danielpuerta0926@gmail.com>
2025-06-11 21:22:20 -0500

    View login finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:acumulapp/views/InicioClienteView.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,116 +8,125 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: Inicio());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Inicio> createState() => _InicioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Scaffold(body: cuerpo(context));
+  }
+}
+
+Widget cuerpo(BuildContext context) {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  return Container(
+    decoration: BoxDecoration(color: Colors.white),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        nombre(),
+        SizedBox(height: 160),
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          alignment: Alignment.centerLeft,
+          child: Text("Email", style: TextStyle(fontSize: 15)),
         ),
+        campoEmail(_emailController),
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          alignment: Alignment.centerLeft,
+          child: Text("Password", style: TextStyle(fontSize: 15)),
+        ),
+        campoContrasena(_passwordController),
+        SizedBox(height: 30),
+        botonEntrar(_emailController, _passwordController, context),
+        SizedBox(height: 30),
+        botonGoogle(),
+      ],
+    ),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "AcumulApp",
+    style: TextStyle(
+      color: Color(0xFF212121),
+      fontSize: 35,
+      fontFamily: 'sans-serif',
+    ),
+  );
+}
+
+Widget campoEmail(TextEditingController _emailController) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+    child: TextField(
+      controller: _emailController,
+      decoration: InputDecoration(fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget campoContrasena(TextEditingController _passwordController) {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+    child: TextField(
+      controller: _passwordController,
+      obscureText: true,
+      decoration: InputDecoration(fillColor: Colors.white, filled: true),
+    ),
+  );
+}
+
+Widget botonEntrar(
+  TextEditingController _emailController,
+  TextEditingController _passwordController,
+  BuildContext context,
+) {
+  return Container(
+    width: double.infinity,
+    padding: EdgeInsets.symmetric(horizontal: 20),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+      onPressed: () {
+        if (("Daniel") == _emailController.text &&
+            ("1234") == _passwordController.text) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Inicioclienteview()),
+          );
+        }
+      },
+      child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 15)),
+    ),
+  );
+}
+
+Widget botonGoogle() {
+  return Container(
+    width: double.infinity,
+    padding: EdgeInsets.symmetric(horizontal: 20),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+      onPressed: () {},
+      child: Text(
+        "Login with Google",
+        style: TextStyle(color: Colors.white, fontSize: 15),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
 }
---
+++ lib/views/InicioClienteView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Inicioclienteview extends StatefulWidget {
+  const Inicioclienteview({super.key});
+
+  @override
+  State<Inicioclienteview> createState() => _InicioclienteviewState();
+}
+
+class _InicioclienteviewState extends State<Inicioclienteview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
