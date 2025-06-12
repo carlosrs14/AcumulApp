1afc475978444c1c8e567a8c71337b7258c0abfd
carlosrs14 <crincones578@gmail.com>
2025-06-11 20:51:39 -0500

    ultimo mapper

--- lib/models/BusinessModel.dart
+++ lib/models/BusinessModel.dart
@@ -7,7 +7,7 @@ class BusinessModel {
   String _direction;
   String _logoUrl;
   List<LinkModel> _links;
-  UbicationModel _ubication;
+  UbicationModel? _ubication;
 
   BusinessModel(
     this._id,
@@ -33,6 +33,40 @@ class BusinessModel {
   List<LinkModel> get links => _links;
   set links(List<LinkModel> value) => _links = value;
   
-  UbicationModel get ubication => _ubication;
-  set ubication(UbicationModel value) => _ubication = value;
+  UbicationModel? get ubication => _ubication;
+  set ubication(UbicationModel? value) => _ubication = value;
+
+  factory BusinessModel.fromJson(Map<String, dynamic> json) {
+    List<LinkModel> linksList = [];
+    if (json['links'] != null) {
+      final l = json['links'] as List;
+      linksList = l.map((item) => LinkModel.fromJson(item as Map<String, dynamic>)).toList();
+    }
+
+    UbicationModel? ubication;
+    if (json.containsKey('ubication') && json['ubication'] != null) {
+      ubication = UbicationModel.fromJson(json['ubication'] as Map<String, dynamic>);
+    }
+
+    return BusinessModel(
+      json['id'] as int,
+      json['name'] as String,
+      json['direction'] as String,
+      json['logoUrl'] as String,
+      linksList,
+      ubication,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': _id,
+      'name': _name,
+      'direction': _direction,
+      'logoUrl': _logoUrl,
+      'links': _links.map((link) => link.toJson()).toList(),
+      // Solo incluyo ubication si no es null
+      if (_ubication != null) 'ubication': _ubication!.toJson(),
+    };
+  }
 }
